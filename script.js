6440c9f8c9118258469524b67491e17074284e40
Armando.Rod <armando.rod79@gmail.com>
2022-12-17 15:22:45 -0800

    update files

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+var weatherApiRootUrl = 'https://api.openweathermap.org';
+var APIKey = "11966b9c99b9aac332f04418ebd9ef84";
+
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
 function GetInfo(){
     var newName=document.getElementById('cityInput');
     var cityName=document.getElementById('cityname');
