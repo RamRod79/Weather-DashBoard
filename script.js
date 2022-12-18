6e86a5a1d375bafa1122c8c4d92c5a193da36472
Armando.Rod <armando.rod79@gmail.com>
2022-12-17 17:23:43 -0800

    factored .html and getInfo in .js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// root url and API key
 var weatherApiRootUrl = 'https://api.openweathermap.org';
 var APIKey = "11966b9c99b9aac332f04418ebd9ef84";
 
@@ -6,7 +7,44 @@ var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&ap
 function GetInfo(){
     var newName=document.getElementById('cityInput');
     var cityName=document.getElementById('cityname');
-    cityName.innerHTML = '--' + newName.value + "--"
+    cityName.innerHTML = "--"+newName.value+"--"
+
+fetch ("https://api.openweathermap.org/data/2.5/forecast?q='+newName.value+'&appid=11966b9c99b9aac332f04418ebd9ef84")
+.then(response => response,json())
+.then(data => {
+    
+    // min/max values for 5 days
+    for (i=0;i<5;i++){
+        document.getElementById("day" +(i+1)+"Min").innerHTML ="Min:" +Number(data.list[i].main.temp_min - 273.15).toFixed(1)+ "°";
+    }
+    for(i = 0; i<5; i++){
+        document.getElementById("day" + (i+1) + "Max").innerHTML = "Max: " + Number(data.list[i].main.temp_max - 273.15).toFixed(2) + "°";
+    }
+
+    //Getting Weather Icons
+     for(i = 0; i<5; i++){
+        document.getElementById("img" + (i+1)).src = "http://openweathermap.org/img/wn/"+
+        data.list[i].weather[0].icon+".png";
+    }
+  })
+
+.catch(err => alert("You did something wrong"))
 }
 
-fetch
+//Days of the Week
+const d = new Date();
+const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday",];
+
+//days array
+function CheckDay(day){
+    if(day + d.getDay() > 6){
+        return day + d.getDay() - 7;
+    }
+    else{
+        return day + d.getDay();
+    }
+}
+
+    for(i = 0; i<5; i++){
+        document.getElementById("day" + (i+1)).innerHTML = weekday[CheckDay(i)];
+    }
